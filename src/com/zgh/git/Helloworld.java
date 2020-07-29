69fc89a10df4a9772c503b0bc850564c28af1a1e
zhuguanghong <zhuprogramer@163.com>
2020-07-30 00:20:08 +0800

    分支提交代码

--- src/com/zgh/git/Helloworld.java
+++ src/com/zgh/git/Helloworld.java
@@ -5,5 +5,6 @@ public class Helloworld {
         System.out.println("1");
         System.out.println("checkout1 添加代码");
         System.out.println(" 分支合代码");
+        System.out.println("frommater_checkout1 提交代码");
     }
 }
