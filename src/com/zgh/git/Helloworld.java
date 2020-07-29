614fedd58705fd794365b84729814a580308fd4c
zhuguanghong <zhuprogramer@163.com>
2020-07-29 08:41:19 +0800

    【2】 增加checkout输出

--- src/com/zgh/git/Helloworld.java
+++ src/com/zgh/git/Helloworld.java
@@ -3,5 +3,6 @@ package com.zgh.git;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("checkout1 添加代码");
     }
 }
